865b2e2791ea7b62dfdd1baf1e175cec24f10de7
YashashreeKolhe <yashashreekolhe@gmail.com>
2019-07-16 07:56:47 -0400

    Changed Load test dataset and added Average Traded Price with its test

---
+++ src/main/java/com/cs/rfq/decorator/extractors/AverageTradedPriceOverPastWeek.java
@@ -0,0 +1,37 @@
+package com.cs.rfq.decorator.extractors;
+
+import com.cs.rfq.decorator.Rfq;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.joda.time.DateTime;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AverageTradedPriceOverPastWeek implements RfqMetadataExtractor{
+
+
+    @Override
+    public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
+        long todayMs = DateTime.now().withMillisOfDay(0).getMillis();
+        long pastWeekMs = DateTime.now().withMillis(todayMs).minusWeeks(1).getMillis();
+
+        String query = String.format("SELECT avg(LastPx) from trade where EntityId='%s' AND SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getEntityId(),
+                rfq.getIsin(),
+                pastWeekMs);
+
+        trades.createOrReplaceTempView("trade");
+        Dataset<Row> sqlQueryResults = session.sql(query);
+
+        Object averageTradedPrice = sqlQueryResults.first().get(0);
+        if (averageTradedPrice == null) {
+            averageTradedPrice = 0.0;
+        }
+
+        Map<RfqMetadataFieldNames, Object> results = new HashMap<>();
+        results.put(RfqMetadataFieldNames.averageTradedPriceOverPastWeek, averageTradedPrice);
+        return results;
+    }
+}
---
+++ src/test/java/com/cs/rfq/decorator/extractors/AverageTradedPriceOverPastWeekTest.java
@@ -0,0 +1,39 @@
+package com.cs.rfq.decorator.extractors;
+
+import com.cs.rfq.decorator.Rfq;
+import com.cs.rfq.decorator.TradeDataLoader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class AverageTradedPriceOverPastWeekTest extends AbstractSparkUnitTest{
+
+    @Test
+    public void AverageTradedPriceWhenTradesPresent() {
+        String filePath = getClass().getResource("loader-test-trades.json").getPath();
+
+        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
+
+        String validRfqJson = "{" +
+                "'id': '123ABC', " +
+                "'traderId': 3351266293154445953, " +
+                "'entityId': 5561279226039690843, " +
+                "'instrumentId': 'AT0000A0VRQ6', " +
+                "'qty': 250000, " +
+                "'price': 1.58, " +
+                "'side': 'B' " +
+                "}";
+
+        Rfq rfq = Rfq.fromJson(validRfqJson);
+
+        AverageTradedPriceOverPastWeek AverageTradedPrice = new AverageTradedPriceOverPastWeek();
+
+        Map<RfqMetadataFieldNames, Object> averageTradedPrice = AverageTradedPrice.extractMetaData(rfq, session, trades);
+
+        assertEquals((Double) 0.0, averageTradedPrice.get(RfqMetadataFieldNames.averageTradedPriceOverPastWeek));
+    }
+}
