b8e6a844157f9533837f92cdf646a3d0df5010ea
YashashreeKolhe <yashashreekolhe@gmail.com>
2019-07-17 00:53:33 -0400

    Added test cases in AverageTradedPriceOverPastWeekTest

--- src/test/java/com/cs/rfq/decorator/AverageTradedPriceOverPastWeekTest.java
+++ src/test/java/com/cs/rfq/decorator/AverageTradedPriceOverPastWeekTest.java
@@ -5,8 +5,12 @@ import com.cs.rfq.decorator.TradeDataLoader;
 import com.cs.rfq.decorator.extractors.AbstractSparkUnitTest;
 import com.cs.rfq.decorator.extractors.AverageTradedPriceOverPastWeek;
 import com.cs.rfq.decorator.extractors.RfqMetadataFieldNames;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.MalformedJsonException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Map;
@@ -15,11 +19,20 @@ import static org.junit.Assert.assertEquals;
 
 public class AverageTradedPriceOverPastWeekTest extends AbstractSparkUnitTest {
 
+    private String filePath = null;
+    private Dataset<Row> trades = null;
+    private AverageTradedPriceOverPastWeek averageTradedPrice = null;
+
+    @Before
+    public void setup() {
+
+        filePath = getClass().getResource("loader-test-trades.json").getPath();
+        trades = new TradeDataLoader().loadTrades(session, filePath);
+        averageTradedPrice = new AverageTradedPriceOverPastWeek();
+    }
+
     @Test
     public void AverageTradedPriceWhenTradesPresent() {
-        String filePath = getClass().getResource("loader-test-trades.json").getPath();
-
-        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
 
         String validRfqJson = "{" +
                 "'id': '123ABC', " +
@@ -33,10 +46,69 @@ public class AverageTradedPriceOverPastWeekTest extends AbstractSparkUnitTest {
 
         Rfq rfq = Rfq.fromJson(validRfqJson);
 
-        AverageTradedPriceOverPastWeek AverageTradedPrice = new AverageTradedPriceOverPastWeek();
+        Map<RfqMetadataFieldNames, Object> result = averageTradedPrice.extractMetaData(rfq, session, trades);
 
-        Map<RfqMetadataFieldNames, Object> averageTradedPrice = AverageTradedPrice.extractMetaData(rfq, session, trades);
+        assertEquals(139.857, result.get(RfqMetadataFieldNames.averageTradedPriceOverPastWeek));
+    }
 
-        assertEquals((Double) 139.857, averageTradedPrice.get(RfqMetadataFieldNames.averageTradedPriceOverPastWeek));
+    @Test
+    public void AverageTradedPriceWhenInstrumentAbsent() {
+        String validRfqJson = "{" +
+                "'id': '123ABC', " +
+                "'traderId': 3351266293154445953, " +
+                "'entityId': 5561279226039690843, " +
+                "'instrumentId': 'AT0000386115', " +
+                "'qty': 250000, " +
+                "'price': 1.58, " +
+                "'side': 'B' " +
+                "}";
+
+        Rfq rfq = Rfq.fromJson(validRfqJson);
+
+        Map<RfqMetadataFieldNames, Object> result = averageTradedPrice.extractMetaData(rfq, session, trades);
+
+        assertEquals(0.0, result.get(RfqMetadataFieldNames.averageTradedPriceOverPastWeek));
+    }
+
+    @Test
+    public void AverageTradedPriceWhenEntityAbsent() {
+        String validRfqJson = "{" +
+                "'id': '123ABC', " +
+                "'traderId': 3351266293154445953, " +
+                "'entityId': 5561279226039690943, " +
+                "'instrumentId': 'AT0000386115', " +
+                "'qty': 250000, " +
+                "'price': 1.58, " +
+                "'side': 'B' " +
+                "}";
+
+        Rfq rfq = Rfq.fromJson(validRfqJson);
+
+        Map<RfqMetadataFieldNames, Object> result = averageTradedPrice.extractMetaData(rfq, session, trades);
+
+        assertEquals(0.0, result.get(RfqMetadataFieldNames.averageTradedPriceOverPastWeek));
+    }
+
+    @Test(expected = JsonSyntaxException.class)
+    public void AverageTradedPriceWhenRfqIncomplete() {
+        String validRfqJson = "{" +
+                "'id': '123ABC', " +
+                "'traderId': 3351266293154445953, " +
+                "'entityId': , " +
+                "'instrumentId': , " +
+                "'qty': 250000, " +
+                "'price': 1.58, " +
+                "'side': 'B' " +
+                "}";
+
+        Rfq rfq = Rfq.fromJson(validRfqJson);
+
+        Map<RfqMetadataFieldNames, Object> result = averageTradedPrice.extractMetaData(rfq, session, trades);
+    }
+
+    @After
+    public void teardown() {
+        trades = null;
+        averageTradedPrice = null;
     }
 }
